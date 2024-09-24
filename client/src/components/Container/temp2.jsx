33f26e611fbcef74c5514f83ed7580b2cac215c6
Bhupendra-17 <bhupendrad1724@gmail.com>
2024-09-24 22:13:35 +0530

    some changes in button component

--- client/src/components/Container/temp2.jsx
+++ client/src/components/Container/temp2.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function temp2(props) {
   return (
-    <div className='text-lg font-semibold border border-gray-400 rounded-full px-2 py-1 w-fit cursor-pointer transition transform duration-300 hover:bg-gray-500 hover:text-white hover:translate-y-0.5 shadow-md'>
+    <div className='text-lg text-center font-semibold border border-gray-400 rounded-full px-2 py-1 w-fit cursor-pointer transition transform duration-300 hover:bg-gray-500 hover:text-white hover:translate-y-0.5 shadow-md'>
         {props.title}
     </div>
   )
